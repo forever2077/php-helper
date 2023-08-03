75ccffc55c6801739ee34a5f66fd21357e267fdd
Jack <rayhaijie@qq.com>
2023-08-03 17:24:45 +0800

    增加两个辅助函数：dd、dump

--- src/functions.php
+++ src/functions.php
@@ -1,1 +1,33 @@
 <?php
+
+namespace Forever2077\PhpHelper;
+
+/**
+ * 支持多个参数
+ * 格式化打印数据
+ */
+function dump(): void
+{
+    $args = func_get_args();
+    foreach ($args as $val) {
+        if ('cli' !== php_sapi_name()) {
+            echo '<pre style="font-size: 14px;">';
+        }
+        print_r($val);
+        if ('cli' !== php_sapi_name()) {
+            echo '</pre>';
+        } else {
+            echo PHP_EOL;
+        }
+    }
+}
+
+/**
+ * 支持多个参数
+ * 格式化打印数据 下断点
+ */
+function dd(): void
+{
+    call_user_func_array(__NAMESPACE__ . '\\dump', func_get_args());
+    exit;
+}
