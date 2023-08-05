91aef6be0ce6452c5aa7b9fa74114d969fe43f8d
Jack <rayhaijie@qq.com>
2023-08-05 22:15:44 +0800

    1、设置 functions.php 为全局函数

--- src/functions.php
+++ src/functions.php
@@ -1,23 +1,23 @@
 <?php
 
-namespace Forever2077\PhpHelper;
-
 /**
  * 支持多个参数，格式化打印数据
  * @return void
  */
-function dump(): void
-{
-    $args = func_get_args();
-    foreach ($args as $val) {
-        if ('cli' !== php_sapi_name()) {
-            echo '<pre style="font-size: 14px;">';
-        }
-        print_r($val);
-        if ('cli' !== php_sapi_name()) {
-            echo '</pre>';
-        } else {
-            echo PHP_EOL;
+if (!function_exists('dump')) {
+    function dump(): void
+    {
+        $args = func_get_args();
+        foreach ($args as $val) {
+            if ('cli' !== php_sapi_name()) {
+                echo '<pre style="font-size: 14px;">';
+            }
+            print_r($val);
+            if ('cli' !== php_sapi_name()) {
+                echo '</pre>';
+            } else {
+                echo PHP_EOL;
+            }
         }
     }
 }
@@ -26,10 +26,12 @@ function dump(): void
  * 支持多个参数，格式化打印数据 下断点
  * @return void
  */
-function dd(): void
-{
-    call_user_func_array(__NAMESPACE__ . '\\dump', func_get_args());
-    exit;
+if (!function_exists('dd')) {
+    function dd(): void
+    {
+        call_user_func_array(__NAMESPACE__ . '\\dump', func_get_args());
+        exit;
+    }
 }
 
 /**
