5fab239231cbe3502fd3d0eda0d19cf6d07bbfa0
Jack <rayhaijie@qq.com>
2023-08-05 23:08:15 +0800

    1、修改说明文档 2、编写 EmailHelper 实体逻辑 3、编写 EmailHelper 测试用例

--- src/EmailHelper.php
+++ src/EmailHelper.php
@@ -2,7 +2,27 @@
 
 namespace Forever2077\PhpHelper;
 
+use PHPMailer\PHPMailer\PHPMailer;
+
 class EmailHelper
 {
+    /**
+     * 默认实例
+     * @param mixed|null $config
+     * @return PHPMailer
+     */
+    public static function instance(mixed $config = null): PHPMailer
+    {
+        return self::phpMailer($config);
+    }
 
+    /**
+     * PHPMailer实例
+     * @param mixed|null $config
+     * @return PHPMailer
+     */
+    public static function phpMailer(mixed $config = null): PHPMailer
+    {
+        return new PHPMailer($config);
+    }
 }
