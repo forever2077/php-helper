4bfdfed2e1999acd46dfe8cc7fe765f086c4e16a
Jack <rayhaijie@qq.com>
2023-08-05 14:23:56 +0800

    1、修改说明文档 2、增加 ImageHelper 中 Intervention 扩展 3、增加 DateTimeHelper 的 instance 默认实例

--- src/DateTimeHelper.php
+++ src/DateTimeHelper.php
@@ -9,13 +9,24 @@ use DateTimeZone;
 class DateTimeHelper
 {
     /**
+     * 默认实例
+     * @param DateTimeInterface|string|null $time
+     * @param DateTimeZone|string|null $tz
+     * @return Carbon|null
+     */
+    public static function instance(DateTimeInterface|string $time = null, DateTimeZone|string $tz = null): ?Carbon
+    {
+        return self::carbon($time, $tz);
+    }
+
+    /**
      * Carbon 时间日期处理库
      * @link https://carbon.nesbot.com/docs/
      * @param DateTimeInterface|string|null $time
      * @param DateTimeZone|string|null $tz
      * @return Carbon|null
      */
-    public static function Carbon(DateTimeInterface|string $time = null, DateTimeZone|string $tz = null): ?Carbon
+    public static function carbon(DateTimeInterface|string $time = null, DateTimeZone|string $tz = null): ?Carbon
     {
         return new Carbon($time, $tz);
     }
--- src/ImageHelper.php
+++ src/ImageHelper.php
@@ -2,7 +2,28 @@
 
 namespace Forever2077\PhpHelper;
 
+use Intervention\Image\ImageManager;
+
 class ImageHelper
 {
+    /**
+     * 默认实例
+     * @param array $config 配置
+     * @return object
+     */
+    public static function instance(array $config = []): object
+    {
+        return self::Intervention($config);
+    }
 
+    /**
+     * Intervention 实例
+     * @link https://image.intervention.io/v2/
+     * @param array $config 配置
+     * @return ImageManager
+     */
+    public static function intervention(array $config = []): ImageManager
+    {
+        return new ImageManager($config);
+    }
 }
