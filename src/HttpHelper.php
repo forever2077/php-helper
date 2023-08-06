18af62180224bedacefafd18a4f6249f30300990
Jack <rayhaijie@qq.com>
2023-08-06 11:00:48 +0800

    1、修改说明文档 2、编写 HttpHelper 实体逻辑 3、编写 HttpHelper 测试用例

--- src/HttpHelper.php
+++ src/HttpHelper.php
@@ -3,12 +3,15 @@
 namespace Forever2077\PhpHelper;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Psr\Http\Message\ResponseInterface;
 
 class HttpHelper
 {
     /**
-     * HTTP辅助类
-     * @param array $config 配置
+     * 返回一个 Guzzle HTTP 客户端实例
+     *
+     * @param array $config 客户端配置项，参见 Guzzle 官方文档
      * @return Client
      */
     public static function instance(array $config = []): Client
@@ -17,12 +20,165 @@ class HttpHelper
     }
 
     /**
-     * Guzzle实例
-     * @param array $config 配置
+     * 返回一个 Guzzle HTTP 客户端实例
+     *
+     * @param array $config 客户端配置项，参见 Guzzle 官方文档
      * @return Client
      */
     public static function guzzle(array $config = []): Client
     {
         return new Client($config);
+    }
+
+    /**
+     * 发送 GET 请求
+     *
+     * @param array $params 包含以下键值：
+     *    - 'client'  (Client) Guzzle客户端实例，默认为新的客户端实例
+     *    - 'url'     (string) 请求的 URL
+     *    - 'options' (array)  请求的额外选项，参见 Guzzle 官方文档
+     * @return ResponseInterface
+     * @throws GuzzleException
+     */
+    public static function get(array $params): ResponseInterface
+    {
+        $client = $params['client'] ?? self::instance();
+        $url = $params['url'] ?? '';
+        $options = $params['options'] ?? [];
+
+        return $client->request('GET', $url, $options);
+    }
+
+    /**
+     * 发送 POST 请求
+     *
+     * @param array $params 包含以下键值：
+     *    - 'client'  (Client) Guzzle客户端实例，默认为新的客户端实例
+     *    - 'url'     (string) 请求的 URL
+     *    - 'options' (array)  请求的额外选项，参见 Guzzle 官方文档
+     * @return ResponseInterface
+     * @throws GuzzleException
+     */
+    public static function post(array $params): ResponseInterface
+    {
+        $client = $params['client'] ?? self::instance();
+        $url = $params['url'] ?? '';
+        $options = $params['options'] ?? [];
+
+        return $client->request('POST', $url, $options);
+    }
+
+    /**
+     * 发送 PUT 请求
+     *
+     * @param array $params 包含以下键值：
+     *    - 'client'  (Client) Guzzle客户端实例，默认为新的客户端实例
+     *    - 'url'     (string) 请求的 URL
+     *    - 'options' (array)  请求的额外选项，参见 Guzzle 官方文档
+     * @return ResponseInterface
+     * @throws GuzzleException
+     */
+    public static function put(array $params): ResponseInterface
+    {
+        $client = $params['client'] ?? self::instance();
+        $url = $params['url'] ?? '';
+        $options = $params['options'] ?? [];
+
+        return $client->request('PUT', $url, $options);
+    }
+
+    /**
+     * 发送 DELETE 请求
+     *
+     * @param array $params 包含以下键值：
+     *    - 'client'  (Client) Guzzle客户端实例，默认为新的客户端实例
+     *    - 'url'     (string) 请求的 URL
+     *    - 'options' (array)  请求的额外选项，参见 Guzzle 官方文档
+     * @return ResponseInterface
+     * @throws GuzzleException
+     */
+    public static function delete(array $params): ResponseInterface
+    {
+        $client = $params['client'] ?? self::instance();
+        $url = $params['url'] ?? '';
+        $options = $params['options'] ?? [];
+
+        return $client->request('DELETE', $url, $options);
+    }
+
+    /**
+     * 发送 HEAD 请求
+     *
+     * @param array $params 包含以下键值：
+     *    - 'client'  (Client) Guzzle客户端实例，默认为新的客户端实例
+     *    - 'url'     (string) 请求的 URL
+     *    - 'options' (array)  请求的额外选项，参见 Guzzle 官方文档
+     * @return ResponseInterface
+     * @throws GuzzleException
+     */
+    public static function head(array $params): ResponseInterface
+    {
+        $client = $params['client'] ?? self::instance();
+        $url = $params['url'] ?? '';
+        $options = $params['options'] ?? [];
+
+        return $client->request('HEAD', $url, $options);
+    }
+
+    /**
+     * 发送 OPTIONS 请求
+     *
+     * @param array $params 包含以下键值：
+     *    - 'client'  (Client) Guzzle客户端实例，默认为新的客户端实例
+     *    - 'url'     (string) 请求的 URL
+     *    - 'options' (array)  请求的额外选项，参见 Guzzle 官方文档
+     * @return ResponseInterface
+     * @throws GuzzleException
+     */
+    public static function options(array $params): ResponseInterface
+    {
+        $client = $params['client'] ?? self::instance();
+        $url = $params['url'] ?? '';
+        $options = $params['options'] ?? [];
+
+        return $client->request('OPTIONS', $url, $options);
+    }
+
+    /**
+     * 发送 PATCH 请求
+     *
+     * @param array $params 包含以下键值：
+     *    - 'client'  (Client) Guzzle客户端实例，默认为新的客户端实例
+     *    - 'url'     (string) 请求的 URL
+     *    - 'options' (array)  请求的额外选项，参见 Guzzle 官方文档
+     * @return ResponseInterface
+     * @throws GuzzleException
+     */
+    public static function patch(array $params): ResponseInterface
+    {
+        $client = $params['client'] ?? self::instance();
+        $url = $params['url'] ?? '';
+        $options = $params['options'] ?? [];
+
+        return $client->request('PATCH', $url, $options);
+    }
+
+    /**
+     * 发送 TRACE 请求
+     *
+     * @param array $params 包含以下键值：
+     *    - 'client'  (Client) Guzzle客户端实例，默认为新的客户端实例
+     *    - 'url'     (string) 请求的 URL
+     *    - 'options' (array)  请求的额外选项，参见 Guzzle 官方文档
+     * @return ResponseInterface
+     * @throws GuzzleException
+     */
+    public static function trace(array $params): ResponseInterface
+    {
+        $client = $params['client'] ?? self::instance();
+        $url = $params['url'] ?? '';
+        $options = $params['options'] ?? [];
+
+        return $client->request('TRACE', $url, $options);
     }
 }
--- tests/HttpHelperTest.php
+++ tests/HttpHelperTest.php
@@ -18,4 +18,115 @@ class HttpHelperTest extends TestCase
             echo $e->getMessage();
         }
     }
+
+    public function testGuzzle()
+    {
+        try {
+            $client = HttpHelper::guzzle();
+            $response = $client->request('GET', 'https://www.baidu.com', [
+                'verify' => __DIR__ . '/cacert.pem',
+            ]);
+            $this->assertEquals(200, $response->getStatusCode());
+        } catch (GuzzleException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public function testGet()
+    {
+        try {
+            $response = HttpHelper::get([
+                'url' => 'https://www.baidu.com',
+                'options' => [
+                    'verify' => __DIR__ . '/cacert.pem',
+                ],
+            ]);
+            $this->assertEquals(200, $response->getStatusCode());
+        } catch (GuzzleException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public function testPost()
+    {
+        try {
+            $response = HttpHelper::post([
+                'url' => 'https://www.baidu.com',
+                'options' => [
+                    'verify' => __DIR__ . '/cacert.pem',
+                ],
+            ]);
+            $this->assertEquals(200, $response->getStatusCode());
+        } catch (GuzzleException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public function testGetWithClient()
+    {
+        try {
+            $client = HttpHelper::guzzle();
+            $response = HttpHelper::get([
+                'client' => $client,
+                'url' => 'https://www.baidu.com',
+                'options' => [
+                    'verify' => __DIR__ . '/cacert.pem',
+                ],
+            ]);
+            $this->assertEquals(200, $response->getStatusCode());
+        } catch (GuzzleException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public function testPostWithClient()
+    {
+        try {
+            $client = HttpHelper::guzzle();
+            $response = HttpHelper::post([
+                'client' => $client,
+                'url' => 'https://www.baidu.com',
+                'options' => [
+                    'verify' => __DIR__ . '/cacert.pem',
+                ],
+            ]);
+            $this->assertEquals(200, $response->getStatusCode());
+        } catch (GuzzleException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public function testGetWithClientAndUrlAndOptions()
+    {
+        try {
+            $client = HttpHelper::guzzle();
+            $response = HttpHelper::get([
+                'client' => $client,
+                'url' => 'https://www.baidu.com',
+                'options' => [
+                    'verify' => __DIR__ . '/cacert.pem',
+                ],
+            ]);
+            $this->assertEquals(200, $response->getStatusCode());
+        } catch (GuzzleException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public function testPostWithClientAndUrlAndOptions()
+    {
+        try {
+            $client = HttpHelper::guzzle();
+            $response = HttpHelper::post([
+                'client' => $client,
+                'url' => 'https://www.baidu.com',
+                'options' => [
+                    'verify' => __DIR__ . '/cacert.pem',
+                ],
+            ]);
+            $this->assertEquals(200, $response->getStatusCode());
+        } catch (GuzzleException $e) {
+            echo $e->getMessage();
+        }
+    }
 }
