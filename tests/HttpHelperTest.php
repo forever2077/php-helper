e6de2a97fefca9d7f4063c3b4e8c0251df2aad49
Jack <rayhaijie@qq.com>
2023-08-06 11:19:14 +0800

    1、修改 HttpHelperTest

--- tests/HttpHelperTest.php
+++ tests/HttpHelperTest.php
@@ -6,32 +6,6 @@ use GuzzleHttp\Exception\GuzzleException;
 
 class HttpHelperTest extends TestCase
 {
-    public function testInstance()
-    {
-        try {
-            $client = HttpHelper::instance();
-            $response = $client->request('GET', 'https://httpbin.org', [
-                'verify' => __DIR__ . '/cacert.pem',
-            ]);
-            $this->assertEquals(200, $response->getStatusCode());
-        } catch (GuzzleException $e) {
-            echo $e->getMessage();
-        }
-    }
-
-    public function testGuzzle()
-    {
-        try {
-            $client = HttpHelper::guzzle();
-            $response = $client->request('GET', 'https://www.baidu.com', [
-                'verify' => __DIR__ . '/cacert.pem',
-            ]);
-            $this->assertEquals(200, $response->getStatusCode());
-        } catch (GuzzleException $e) {
-            echo $e->getMessage();
-        }
-    }
-
     public function testGet()
     {
         try {
@@ -85,70 +59,6 @@ class HttpHelperTest extends TestCase
             $client = HttpHelper::guzzle();
             $response = HttpHelper::post([
                 'client' => $client,
-                'url' => 'https://www.baidu.com',
-                'options' => [
-                    'verify' => __DIR__ . '/cacert.pem',
-                ],
-            ]);
-            $this->assertEquals(200, $response->getStatusCode());
-        } catch (GuzzleException $e) {
-            echo $e->getMessage();
-        }
-    }
-
-    public function testGetWithClientAndUrlAndOptions()
-    {
-        try {
-            $client = HttpHelper::guzzle();
-            $response = HttpHelper::get([
-                'client' => $client,
-                'url' => 'https://www.baidu.com',
-                'options' => [
-                    'verify' => __DIR__ . '/cacert.pem',
-                ],
-            ]);
-            $this->assertEquals(200, $response->getStatusCode());
-        } catch (GuzzleException $e) {
-            echo $e->getMessage();
-        }
-    }
-
-    public function testPostWithClientAndUrlAndOptions()
-    {
-        try {
-            $client = HttpHelper::guzzle();
-            $response = HttpHelper::post([
-                'client' => $client,
-                'url' => 'https://www.baidu.com',
-                'options' => [
-                    'verify' => __DIR__ . '/cacert.pem',
-                ],
-            ]);
-            $this->assertEquals(200, $response->getStatusCode());
-        } catch (GuzzleException $e) {
-            echo $e->getMessage();
-        }
-    }
-
-    public function testSendJson()
-    {
-        try {
-            $response = HttpHelper::sendJson([
-                'url' => 'https://www.baidu.com',
-                'options' => [
-                    'verify' => __DIR__ . '/cacert.pem',
-                ],
-            ]);
-            $this->assertEquals(200, $response->getStatusCode());
-        } catch (GuzzleException $e) {
-            echo $e->getMessage();
-        }
-    }
-
-    public function testSendForm()
-    {
-        try {
-            $response = HttpHelper::sendForm([
                 'url' => 'https://www.baidu.com',
                 'options' => [
                     'verify' => __DIR__ . '/cacert.pem',
