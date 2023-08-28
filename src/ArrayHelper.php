cb528867c30bce15a8e14609a39133beee9ec306
Jack <rayhaijie@qq.com>
2023-08-28 13:27:01 +0800

    1、编写 ArrayHelper 实体逻辑 2、编写 ArrayHelperTest 实体逻辑 3、修改说明文档

--- src/ArrayHelper.php
+++ src/ArrayHelper.php
@@ -2,52 +2,13 @@
 
 namespace Forever2077\PhpHelper;
 
-class ArrayHelper
+use Aimeos\Map;
+
+class ArrayHelper extends Map
 {
-    /**
-     * 把对象或者数组对象，转成数组
-     * @param $object
-     * @param array $properties
-     * @param bool $recursive
-     * @return array|object[]|string[]
-     */
-    public static function toArray($object, array $properties = [], bool $recursive = true): array
+    public function __construct($elements = [])
     {
-        if (is_array($object)) {
-            if ($recursive) {
-                foreach ($object as $key => $value) {
-                    if (is_array($value) || is_object($value)) {
-                        $object[$key] = static::toArray($value, $properties, true);
-                    }
-                }
-            }
-
-            return $object;
-        } elseif (is_object($object)) {
-            if (!empty($properties)) {
-                $className = get_class($object);
-                if (!empty($properties[$className])) {
-                    $result = [];
-                    foreach ($properties[$className] as $key => $name) {
-                        if (is_int($key)) {
-                            $result[$name] = $object->$name;
-                        } else {
-                            $result[$key] = static::getValue($object, $name);
-                        }
-                    }
-
-                    return $recursive ? static::toArray($result, $properties) : $result;
-                }
-            }
-            $result = [];
-            foreach ($object as $key => $value) {
-                $result[$key] = $value;
-            }
-
-            return $recursive ? static::toArray($result, $properties) : $result;
-        } else {
-            return [$object];
-        }
+        parent::__construct($elements);
     }
 
     /**
@@ -92,85 +53,6 @@ class ArrayHelper
     }
 
     /**
-     * 根据指定的key，建立key对应索引的数组，或者分组后的索引数组
-     * @param $array
-     * @param $key
-     * @param array $groups
-     * @return array
-     */
-    public static function index($array, $key, array $groups = []): array
-    {
-        $result = [];
-        $groups = (array)$groups;
-
-        foreach ($array as $element) {
-            $lastArray = &$result;
-
-            foreach ($groups as $group) {
-                $value = static::getValue($element, $group);
-                if (!array_key_exists($value, $lastArray)) {
-                    $lastArray[$value] = [];
-                }
-                $lastArray = &$lastArray[$value];
-            }
-
-            if ($key === null) {
-                if (!empty($groups)) {
-                    $lastArray[] = $element;
-                }
-            } else {
-                $value = static::getValue($element, $key);
-                if ($value !== null) {
-                    if (is_float($value)) {
-                        $value = (string)$value;
-                    }
-                    $lastArray[$value] = $element;
-                }
-            }
-            unset($lastArray);
-        }
-
-        return $result;
-    }
-
-    /**
-     * 把数组转成 key-value 的形式
-     * @param $array
-     * @param $from
-     * @param $to
-     * @param $group
-     * @return array
-     */
-    public static function map($array, $from, $to, $group = null): array
-    {
-        $result = [];
-        foreach ($array as $element) {
-            $key = static::getValue($element, $from);
-            $value = static::getValue($element, $to);
-            if ($group !== null) {
-                $result[static::getValue($element, $group)][$key] = $value;
-            } else {
-                $result[$key] = $value;
-            }
-        }
-
-        return $result;
-    }
-
-    /**
-     * 检查数组是否是列索引
-     * @param array $arr
-     * @return bool
-     */
-    public static function isAssoc(array $arr): bool
-    {
-        if (!$arr) {
-            return false;
-        }
-        return array_keys($arr) !== range(0, count($arr) - 1);
-    }
-
-    /**
      * 递归获取指定下标数组
      * @param array $arr 数组
      * @param string $pid 父级值
@@ -202,5 +84,18 @@ class ArrayHelper
             }
         }
         return $tree;
+    }
+
+    /**
+     * 检查数组是否是列索引
+     * @param array $arr
+     * @return bool
+     */
+    public static function isAssoc(array $arr): bool
+    {
+        if (!$arr) {
+            return false;
+        }
+        return array_keys($arr) !== range(0, count($arr) - 1);
     }
 }
--- tests/ArrayHelperTest.php
+++ tests/ArrayHelperTest.php
@@ -23,10 +23,12 @@ class ArrayHelperTest extends TestCase
         $this->assertEquals(ArrayHelper::Class, Helper::array()::class);
     }
 
-    public function testToArray()
+    public function testMap()
     {
-        $a1 = new Foo();
-        $this->assertEquals(['a' => 1, 'b' => 2], ArrayHelper::toArray($a1));
+        $map = ArrayHelper::from([0 => 'b', 1 => 'a'])->after('b');
+        $this->assertEquals([1 => 'a'], $map->toArray());
+        $map = ArrayHelper::from(['b' => 0, 'a' => 1])->arsort();
+        $this->assertEquals(['a' => 1, 'b' => 0], $map->toArray());
     }
 
     public function testGetValue()
