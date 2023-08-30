507c57fcc0aa9a1a1078be7c707123d704b2a4d2
Jack <rayhaijie@qq.com>
2023-08-30 16:13:51 +0800

    1、修改说明文档

--- tests/PhpHelperTest.php
+++ tests/PhpHelperTest.php
@@ -11,25 +11,28 @@ class PhpHelperTest extends TestCase
         $this->assertIsString(PhpHelper::$version);
     }
 
-    public function testCountPubMethod()
+    private function testDoc()
     {
-        try {
-            $str = PhpHelper::countPubMethod();
-            print_r($str);
-            $this->assertIsString($str);
-        } catch (Exception $e) {
-            dump($e->getMessage());
-        }
-    }
+        echo PHP_EOL;
 
-    public function testQ()
-    {
-        try {
-            $this->assertIsString(Helper::file()->format(1024));
-            $arr = [1, 3, 2, 5, 4];
-            $this->assertIsArray(Helper::algorithm()->BubbleSort($arr));
-        } catch (Exception $e) {
-            dump($e->getMessage());
-        }
+        $path = dirname(__DIR__) . '/doc';
+        Helper::file()::scanDirectory([
+            'dir' => $path,
+            'callback' => function ($file) use ($path) {
+
+                $content = file_get_contents($file);
+                preg_match("/##(.*)/", $content, $match);
+                if (isset($match[1])) {
+                    $title = trim($match[1]);
+                } else {
+                    throw new \Exception($file);
+                }
+
+                $filepath = "doc/" . pathinfo($file, PATHINFO_BASENAME);
+                echo "[{$title}]({$filepath})";
+
+                echo PHP_EOL;
+            }
+        ]);
     }
 }
