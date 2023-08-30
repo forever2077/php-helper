a1af3d94b8244f2d2224b39da8be9d5e19d2809c
Jack <rayhaijie@qq.com>
2023-08-30 18:44:52 +0800

    1、修改说明文档

--- tests/PhpHelperTest.php
+++ tests/PhpHelperTest.php
@@ -11,6 +11,28 @@ class PhpHelperTest extends TestCase
         $this->assertIsString(PhpHelper::$version);
     }
 
+    public function testCountPubMethod()
+    {
+        try {
+            $str = PhpHelper::countPubMethod();
+            print_r($str);
+            $this->assertIsString($str);
+        } catch (Exception $e) {
+            dump($e->getMessage());
+        }
+    }
+
+    public function testQ()
+    {
+        try {
+            $this->assertIsString(Helper::file()->format(1024));
+            $arr = [1, 3, 2, 5, 4];
+            $this->assertIsArray(Helper::algorithm()->BubbleSort($arr));
+        } catch (Exception $e) {
+            dump($e->getMessage());
+        }
+    }
+
     private function testDoc()
     {
         echo PHP_EOL;
